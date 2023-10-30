0b59751fcae5f419a4066b568eb51e8f82382622
MrLo <147477447+MrJLou@users.noreply.github.com>
2023-10-30 19:24:52 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 import './App.css';
-import { Link } from "react-router-dom"
+import logo from '/Clr.png';
+import logo2 from '/A.png';
 import { SearchBar } from "./Components/SearchBar";
 import { SearchResultsList } from "./components/SearchResultsList";
 
 function App() {
 
-  const [results, setResults] = useState([]);
+ const [results, setResults] = useState([]);
 
-  return <div className="App">
-  <Link to="/" className='title'>CLR</Link>
-  <Link to="Log-in" className='user'>Login</Link>
-  <nav>
+ return <div className="App">
+ <nav>
+    <img src={logo} className="title" alt="title" width="70"/>
+    <img src={logo2} className="user" alt="user" width="65"/>
+ 
     <div className="search-bar-container">
         <SearchBar setResults={setResults} />
         {results && results.length > 0 && <SearchResultsList results={results} />}
       </div>
-  </nav>
+ </nav>
   
-  </div>
+ </div>
   
 }
 
